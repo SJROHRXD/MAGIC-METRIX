aac9102ffa9314c20c68a7fa40a47825c9ace1aa
SJRXD <rohr.olasz@gmail.com>
2023-05-05 03:12:58 -0700

    Updating FrontEnd Login Form w validation and state

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,31 +1,62 @@
-import { Form, Button, FormLabel } from "react-bootstrap";
+import { useState } from 'react';
+import { Form, Button, FormLabel, Col, Row, Container } from "react-bootstrap";
+// import { render } from 'react-dom';
+
+// function to handle state + submit
+// validated, setValidated
+// prevent default and stop propagation etc
 
 function Login() {
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+
+        // checkValidity is a built-in method for forms / prop for forms
+        // if the form is invalid, prevent default and stop propagation
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+            }
+
+        setValidated(true);
+    };
+
     return (
         <>
         <h2>Please, Log In</h2>
-        <Form>
-            <Form.Group>
-                <FormLabel>Password</FormLabel>
-                <Form.Control
-                    type="password"
-                    id="inputPassword"
-                    // aria+
-                />
-            </Form.Group>
-            <Form.Group>
-                <FormLabel>Email Address</FormLabel>
-                <Form.Control
-                    type="email"
-                    id="inputEmail"
-                    placeholder="worldsbestdad76@hotmail.com"
-                    // aria+
-                />
-            </Form.Group>
-            <Button>Submit</Button>
-        </Form>
+            <Container>
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                    <Row className="mb-sm-3">
+                        <Form.Group as={Col}>
+                            <FormLabel>Email Address</FormLabel>
+                            <Form.Control
+                                type="email"
+                                id="inputEmail"
+                                placeholder="worldsbestdad76@hotmail.com"
+                                // aria+
+                            />
+                        <Form.Control.Feedback type="invalid">
+                            Please, provide a valid email.
+                        </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group as={Col}>
+                            <FormLabel>Password</FormLabel>
+                            <Form.Control
+                                type="password"
+                                id="inputPassword"
+                                // aria+
+                            />
+                        </Form.Group>
+                    </Row>
+                    <Button type="submit">Submit</Button>
+                </Form>
+            </Container>
         </>
     );
 }
 
 export default Login;
+
+// render(<Login />);
+// only render when testing in isolation
--- vite.config.js
+++ vite.config.js
@@ -15,3 +15,21 @@ export default defineConfig({
     historyApiFallback: true,
   },
 });
+
+// import path from "node:path";
+// import process from "node:process";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   server: {
+//     open: "main.html",
+//   },
+//   root: "src",
+//   publicDir: "../public",
+//   build: {
+//     outDir: "../dist"
+//   },
+//   resolve: {
+//     alias: { "/src": path.resolve(process.cwd(), "src") }
+//   },
+// });
